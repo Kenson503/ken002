15f0fc4e5a010a0fac9d24084a7f374af0f25b7e
Kenson503 <kensonmureithi503@gmail.com>
2025-07-18 12:39:08 +0300

    added main.js for typing animation

---
+++ main.js
@@ -0,0 +1,11 @@
+// Make sure Typed.js is loaded before this script
+// If using a module system, uncomment the next line:
+// import Typed from 'typed.js';
+
+var typed = new Typed(".text", {
+    strings: ["kenson","Ndichu"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+});
